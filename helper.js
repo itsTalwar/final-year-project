4612f7af06d774a8228f563a253b59d9cecba2eb
Nikhil <nktalwar15@gmail.com>
2019-04-23 23:04:05 +0530

    with node server

--- helper.js
+++
@@ -1,9 +0,0 @@
-Function.prototype.bind = function(newThis) {
-    if (typeof this !== "function") {
-      throw new Error(this + "cannot be bound as it's not callable");
-    }
-    var boundTargetFunction = this;
-    return function boundFunction() {
-      return boundTargetFunction.apply(newThis);
-    };
-  };
